5562f44af6e10557037581e80dce990a8e899ceb
CaoDuyPhuoc <phuoccd@aisolutionsjsc.com>
2022-06-19 21:14:09 +0700

    Xây dựng UI Header phần 2

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -10,13 +10,22 @@ import {
     faEarthAsia,
     faCircleQuestion,
     faKeyboard,
+    faUser,
+    faCoins,
+    faGear,
+    faSignOut,
 } from '@fortawesome/free-solid-svg-icons';
-import Tippy from '@tippyjs/react/headless';
+import {
+    faEnvelopeOpen,
+    faComments,
+} from '@fortawesome/free-regular-svg-icons';
+import TippyHeadless from '@tippyjs/react/headless';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
 
 import styles from './Header.module.scss';
 import images from '../../../../assets/images';
 import { Wrapper as PopperWrapper } from '../../../Popper';
-import AccountItem from '../../../AccountItem';
 import Button from '../../../Button';
 import Menu from '../../../Popper/Menu';
 
@@ -26,6 +35,21 @@ const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faEarthAsia} />,
         title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'Language',
+                    code: 'en-US',
+                    title: 'English',
+                },
+                {
+                    type: 'Language',
+                    code: 'vi-VN',
+                    title: 'Tiếng Việt',
+                },
+            ],
+        },
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -38,6 +62,31 @@ const MENU_ITEMS = [
     },
 ];
 
+const USER_MENU = [
+    {
+        icon: <FontAwesomeIcon icon={faUser} />,
+        title: 'View Profile',
+        to: '/viewprofile',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCoins} />,
+        title: 'Get coins',
+        to: '/getcoins',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faGear} />,
+        title: 'Settings',
+        to: '/settings',
+    },
+    ...MENU_ITEMS,
+    {
+        icon: <FontAwesomeIcon icon={faSignOut} />,
+        title: 'Log out',
+        to: '/logout',
+        separate: true,
+    },
+];
+
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
 
@@ -47,11 +96,23 @@ function Header() {
         }, 0);
     }, []);
 
+    const handleMenuChange = (menuItem) => {
+        switch (menuItem.type) {
+            case 'Language':
+                // Handle
+                break;
+            default:
+                break;
+        }
+    };
+
+    const currentUser = true;
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <img src={images.logo} alt="Tiktok" />
-                <Tippy
+                <TippyHeadless
                     interactive
                     visible={searchResult.length > 0}
                     render={(attrs) => (
@@ -62,10 +123,6 @@ function Header() {
                         >
                             <PopperWrapper>
                                 <h4 className={cx('search-title')}>Accounts</h4>
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
                             </PopperWrapper>
                         </div>
                     )}
@@ -86,23 +143,65 @@ function Header() {
                             <FontAwesomeIcon icon={faMagnifyingGlass} />
                         </button>
                     </div>
-                </Tippy>
-
+                </TippyHeadless>
                 <div className={cx('actions')}>
-                    <Button
-                        outlineBlack
-                        leftIcon={<FontAwesomeIcon icon={faPlus} />}
+                    {currentUser ? (
+                        <>
+                            <Button
+                                outlineBlack
+                                leftIcon={<FontAwesomeIcon icon={faPlus} />}
+                            >
+                                Upload
+                            </Button>
+                            <Tippy
+                                content="Message"
+                                delay={[0, 100]}
+                                placement="bottom"
+                            >
+                                <button className={cx('menu-icon-user')}>
+                                    <FontAwesomeIcon icon={faEnvelopeOpen} />
+                                </button>
+                            </Tippy>
+                            <Tippy
+                                content="Inbox"
+                                delay={[0, 100]}
+                                placement="bottom"
+                            >
+                                <button className={cx('menu-icon-user')}>
+                                    <FontAwesomeIcon icon={faComments} />
+                                </button>
+                            </Tippy>
+                        </>
+                    ) : (
+                        <>
+                            <Button
+                                outlineBlack
+                                leftIcon={<FontAwesomeIcon icon={faPlus} />}
+                            >
+                                Upload
+                            </Button>
+                            <Button primary>Log In</Button>
+                        </>
+                    )}
+                    <Menu
+                        items={currentUser ? USER_MENU : MENU_ITEMS}
+                        onChange={handleMenuChange}
                     >
-                        Upload
-                    </Button>
-                    <Button primary>Log In</Button>
-
-                    <Menu items={MENU_ITEMS}>
                         <span className={cx('menu')}>
-                            <FontAwesomeIcon
-                                className={cx('menu-icon')}
-                                icon={faEllipsisV}
-                            />
+                            {currentUser ? (
+                                <>
+                                    <img
+                                        src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-aiso/d9a4e2ab41aec2fb054d05d9b1322e40~c5_100x100.jpeg?x-expires=1655812800&x-signature=bJY36HE6vFWbQXG6mRhrcCI%2F40Y%3D"
+                                        alt="Hoaa"
+                                        className={cx('user-avatar')}
+                                    />
+                                </>
+                            ) : (
+                                <FontAwesomeIcon
+                                    className={cx('menu-icon')}
+                                    icon={faEllipsisV}
+                                />
+                            )}
                         </span>
                     </Menu>
                 </div>
